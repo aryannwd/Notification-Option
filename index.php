ed34cefa734f336076a4ee7348b5c33bd4838e00
Aryan Kumar <aryannwd005@gmail.com>
2017-11-21 01:46:17 +0530

    modified

--- index.php
+++ index.php
@@ -4,11 +4,9 @@
 	<title></title>
 </head>
 <body>
-<<<<<<< HEAD
-   
-=======
+
    <h1>Welcome to MyNearShop</h1>
->>>>>>> 851f172a1bab82e55e1f45f7a37f1ce4eb7cc029
+
 	<?php  include('home.php'); ?>
 </body>
 </html>
