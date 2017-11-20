58163de41373541781f311606d37bd7b84670f41
Aryan Kumar <aryannwd@users.noreply.github.com>
2017-11-21 01:48:06 +0530

    Updated

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Notification Option</title>
 </head>
 <body>
-<<<<<<< HEAD
-   
-=======
+
+
    <h1>Welcome to MyNearShop</h1>
->>>>>>> 851f172a1bab82e55e1f45f7a37f1ce4eb7cc029
+
 	<?php  include('home.php'); ?>
 </body>
 </html>
